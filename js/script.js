4a90005330dc28034b8b10795ab92067c00f80d4
Shukur939 <shukursurkhayli000@gmail.com>
2022-03-19 15:25:25 +0400

    initialcommit

--- js/script.js
+++ js/script.js
@@ -3,20 +3,15 @@ const btnAdd = document.querySelector(".btn");
 
 btnAdd.addEventListener("click",addNew);
 
-
 function addNew(){
   const newDiv = document.createElement("div");
   console.log("add");
   newDiv.classList.add('addDiv');
   document.body.appendChild(newDiv);
-
+  newDiv.addEventListener("click",()=>{
+    newDiv.remove();
+  });
   var bgColor = "#" + Math.round(0xffffff*Math.random()).toString(16);
   newDiv.style.background = bgColor;
 }
 
-
-// function randomRectangles(){
-// ctx.beginPath();
-// ctx.rect(20, 20, 150, 100);
-// ctx.stroke();
-// }
