c75a53b7543073c65f40c677c1bb7f490b64ce9e
Shukur939 <shukursurkhayli000@gmail.com>
2022-03-21 18:32:12 +0400

    initialcommit

--- js/script.js
+++ js/script.js
@@ -5,16 +5,18 @@ addDiv.addEventListener("click", () => {
   document.body.appendChild(newDiv);
 
   var bgColor = "#" + Math.round(0xffffff * Math.random()).toString(16);
-  const x = Math.round(Math.random() * 200 + 20);
-  const y = Math.round(Math.random() * 200 + 20);
-  var randomWid = Math.floor(Math.random() * 300);
-  var randomHei = Math.floor(Math.random() * 300);
+  const x = Math.round(Math.random() * 1000 + 20);
+  const y = Math.round(Math.random() * 200 + 40);
+  const z = Math.round(Math.random() * 300+ 40);
+  var randomWid = Math.floor(Math.random() * 100);
+  var randomHei = Math.floor(Math.random() * 100);
   newDiv.style.background = bgColor;
   newDiv.style.width = randomWid + "px";
   newDiv.style.height = randomHei + "px";
   newDiv.style.position = "absolute";
   newDiv.style.left = x + "px";
   newDiv.style.top = y + "px";
+  newDiv.style.right = z + "px";
   newDiv.style.visibility = "visible";
 
   newDiv.addEventListener("click", () => {
