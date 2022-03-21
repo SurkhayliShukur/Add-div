ed42a5ed0b781e48bf46a1a55bfcb49d4b35b1a1
Shukur939 <shukursurkhayli000@gmail.com>
2022-03-21 15:55:10 +0400

    initialcommit

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
-const btnAdd = document.querySelector(".btn");
+let addDiv = document.querySelector(".btn-success");
 
+addDiv.addEventListener("click", ()=>{
+    const x =Math.floor(Math.random()*500+20);
+    const y =Math.floor(Math.random()*500+20);
+    const newDiv = document.createElement("div");
+    newDiv.style.left = x + "px";
+    newDiv.style.top = y + "px";
+    console.log("add");
+    newDiv.classList.add('AddDiv');
+    document.body.appendChild(newDiv);
 
-btnAdd.addEventListener("click",addNew);
+    var bgColor = "#" + Math.round(0xffffff * Math.random()).toString(16);
+    newDiv.style.background = bgColor;
 
-function addNew(){
-  const newDiv = document.createElement("div");
-  console.log("add");
-  newDiv.classList.add('addDiv');
-  document.body.appendChild(newDiv);
-  newDiv.addEventListener("click",()=>{
-    newDiv.remove();
-  });
-  var bgColor = "#" + Math.round(0xffffff*Math.random()).toString(16);
-  newDiv.style.background = bgColor;
-}
-
+    newDiv.addEventListener("click", () => {
+        newDiv.remove();
+    });
+    var randomWid = Math.floor(Math.random()*100);
+    var randomHei = Math.floor(Math.random()*100);
+    newDiv.style.width = randomWid;
+    newDiv.style.height = randomHei;
+    newDiv.style.visibility = "visible";
+});
