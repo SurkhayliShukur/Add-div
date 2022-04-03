9c64cfad98c052aea021ec624ff80d9ee7fd5c8e
Shukur939 <shukursurkhayli000@gmail.com>
2022-04-03 10:13:16 +0400

    initialcommit

--- js/script.js
+++ js/script.js
@@ -3,15 +3,19 @@ var documentHeight = document.documentElement.clientHeight;
 var documentWidth = document.documentElement.clientWidth;
 
 addDiv.addEventListener("click", () => {
+  // CREATE ELEMENT
   const newDiv = document.createElement("div");
   document.body.appendChild(newDiv);
-
+  // RANDOM COLOR
   var bgColor = "#" + Math.round(0xffffff * Math.random()).toString(16);
-  const newdivSize = Math.floor(Math.random()*80+20);
-  const x = Math.round(Math.random() * (documentWidth)-newdivSize);
-  const y = Math.round(Math.random() * (documentHeight)-newdivSize);
+  // RANDOM SIZE
+  const newdivSize = Math.floor(Math.random() * 80 + 20);
+  const x = Math.round(Math.random() * documentWidth - newdivSize);
+  const y = Math.round(Math.random() * documentHeight - newdivSize);
+  // RANDOM SCREEN LOCATIONS
   var randomWid = Math.floor(Math.random() * 100);
   var randomHei = Math.floor(Math.random() * 100);
+  // RANDOM STYLE
   newDiv.style.background = bgColor;
   newDiv.style.width = randomWid + "px";
   newDiv.style.height = randomHei + "px";
@@ -19,7 +23,7 @@ addDiv.addEventListener("click", () => {
   newDiv.style.left = x + "px";
   newDiv.style.top = y + "px";
   newDiv.style.visibility = "visible";
-
+  // REMOVE DIV
   newDiv.addEventListener("click", () => {
     newDiv.remove();
   });
